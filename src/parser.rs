c8e041e8c29ab23456ca17cda43eea361d19b7cd
Brandon <liuy01510.work@gmail.com>
2022-07-30 02:33:28 +0000

    Created parser struct and the associated methods.

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,56 @@
+//! Contains the parser module used for parsing the tokens into Python dataframe code.
 pub mod parser {
+    use std::error::Error;
+
+    use crate::errors::errors::ParseErr;
     use crate::lexer::lexer::Token;
 
-    pub struct RustyParser {
-        current_token: Token,
-        next_token: Token,
+    /// Parser struct.
+    /// Lifetime of the parser is tied to the lifetime of the source.
+    pub struct RustyParser<'a> {
+        current_token: Option<Token>,
+        next_token: Option<Token>,
+        lexer: logos::Lexer<'a, Token>,
+    }
+
+    impl RustyParser<'_> {
+        /// Returns true if the current token matches the input token. Returns false otherwise.
+        fn check_token(&self, kind: &Token) -> bool {
+            match &self.current_token {
+                Some(kind) => true,
+                _ => false,
+            }
+        }
+
+        /// Returns true if the next token matches the input token. Returns false otherwise.
+        fn check_next_token(&self, kind: &Token) -> bool {
+            match &self.next_token {
+                Some(kind) => true,
+                _ => false,
+            }
+        }
+
+        /// If the current token matches the input token, will advance the current token to the next token.
+        /// Else, will return a ParseErr type.
+        fn match_token(&mut self, kind: &Token) -> Result<(), ParseErr> {
+            match self.check_token(kind) {
+                true => {
+                    self.move_token();
+                    Ok(())
+                }
+                false => match self.current_token.take() {
+                    Some(i) => Err(ParseErr::new(kind.clone(), i)),
+                    None => Err(ParseErr::new(kind.clone(), Token::EOF)),
+                },
+            }
+        }
+
+        /// Replaces the current token with the next token.
+        /// Replaces the next token with the following token.
+        fn move_token(&mut self) {
+            let following_token = self.lexer.next();
+            self.current_token = self.next_token.take();
+            self.next_token = following_token;
+        }
     }
 }
